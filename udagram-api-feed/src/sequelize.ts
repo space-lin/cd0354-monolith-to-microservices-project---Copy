51acb1e177f09a8d024a8d1a4b1a7b688dfa7b63
space-lin <lsa208628@gmail.com>
2024-12-15 23:47:02 +0700

    update db

--- udagram-api-feed/src/sequelize.ts
+++ udagram-api-feed/src/sequelize.ts
@@ -1,5 +1,5 @@
-import {Sequelize} from 'sequelize-typescript';
-import {config} from './config/config';
+import { Sequelize } from 'sequelize-typescript';
+import { config } from './config/config';
 
 
 export const sequelize = new Sequelize({
@@ -10,4 +10,10 @@ export const sequelize = new Sequelize({
 
   'dialect': config.dialect,
   'storage': ':memory:',
+  'dialectOptions': {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false
+    }
+  }
 });
