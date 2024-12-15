52ba3b2f8f017f2f12c0b3546d28ac3e2a806945
space-lin <lsa208628@gmail.com>
2024-12-16 03:12:07 +0700

    humm

--- udagram-api-user/src/server.ts
+++ udagram-api-user/src/server.ts
@@ -1,9 +1,7 @@
 import cors from 'cors';
 import express from 'express';
 import {sequelize} from './sequelize';
-
 import {IndexRouter} from './controllers/v0/index.router';
-
 import bodyParser from 'body-parser';
 import {config} from './config/config';
 import {V0_USER_MODELS} from './controllers/v0/model.index';
@@ -18,33 +16,42 @@ import {V0_USER_MODELS} from './controllers/v0/model.index';
   const app = express();
   const port = process.env.PORT || 8080;
 
+  // Define allowed origins
+  const allowedOrigins = [
+    'http://aff2e432476004eaaa05153ce7e87f92-2065947421.us-east-1.elb.amazonaws.com',  // Your frontend domain
+  ];
+
   app.use(bodyParser.json());
 
-  // We set the CORS origin to * so that we don't need to
-  // worry about the complexities of CORS this lesson. It's
-  // something that will be covered in the next course.
+  // CORS configuration
   app.use(cors({
+    origin: (origin, callback) => {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error('Not allowed by CORS'));
+      }
+    },
     allowedHeaders: [
       'Origin', 'X-Requested-With',
       'Content-Type', 'Accept',
       'X-Access-Token', 'Authorization',
     ],
     methods: 'GET,HEAD,OPTIONS,PUT,PATCH,POST,DELETE',
-    preflightContinue: true,
-    origin: '*',
+    preflightContinue: false, // Automatically handles preflight
   }));
 
+  // Define routes
   app.use('/api/v0/', IndexRouter);
 
   // Root URI call
-  app.get( '/', async ( req, res ) => {
-    res.send( '/api/v0/' );
-  } );
-
+  app.get('/', async (req, res) => {
+    res.send('/api/v0/');
+  });
 
   // Start the Server
-  app.listen( port, () => {
-    console.log( `server running ${config.url}` );
-    console.log( `press CTRL+C to stop server` );
-  } );
+  app.listen(port, () => {
+    console.log(`server running ${config.url}`);
+    console.log('press CTRL+C to stop server');
+  });
 })();
