825d71e9ff5bc7f44b7cabf857eccb69f155a11c
space-lin <lsa208628@gmail.com>
2024-12-16 15:55:25 +0700

    guuu

--- udagram-api-feed/src/server.ts
+++ udagram-api-feed/src/server.ts
@@ -1,13 +1,13 @@
 import cors from 'cors';
 import express from 'express';
-import {sequelize} from './sequelize';
-import {IndexRouter} from './controllers/v0/index.router';
+import { sequelize } from './sequelize';
+import { IndexRouter } from './controllers/v0/index.router';
 import bodyParser from 'body-parser';
-import {config} from './config/config';
-import {V0_FEED_MODELS} from './controllers/v0/model.index';
+import { config } from './config/config';
+import { V0_USER_MODELS } from './controllers/v0/model.index';
 
 (async () => {
-  await sequelize.addModels(V0_FEED_MODELS);
+  await sequelize.addModels(V0_USER_MODELS);
 
   console.debug("Initialize database connection...");
   await sequelize.sync();
@@ -15,21 +15,30 @@ import {V0_FEED_MODELS} from './controllers/v0/model.index';
   const app = express();
   const port = process.env.PORT || 8080;
 
-
-  app.use(bodyParser.json());
-
   // Define allowed origins
-  const allowedOrigins = ['aff2e432476004eaaa05153ce7e87f92-2065947421.us-east-1.elb.amazonaws.com', 'a763c4ab5f15543c5a1a6ecc02a37b4a-1656197018.us-east-1.elb.amazonaws.com'];
+  const allowedOrigins = [
+    'http://aff2e432476004eaaa05153ce7e87f92-2065947421.us-east-1.elb.amazonaws.com/',
+    'http://another-allowed-origin.com'
+  ];
+
   const corsOptions = {
-    origin: (origin: any, callback: any) => {
+    origin: (origin:any, callback:any) => {
       if (allowedOrigins.includes(origin) || !origin) {
         callback(null, true);
       } else {
         callback(new Error('Not allowed by CORS'));
       }
-    }
+    },
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true,
   };
-  
+
+  app.use(bodyParser.json());
+
+  // CORS configuration
+  app.use(cors(corsOptions));
+
   // Define routes
   app.use('/api/v0/', IndexRouter);
 
--- udagram-api-user/src/server.ts
+++ udagram-api-user/src/server.ts
@@ -16,15 +16,22 @@ import { V0_USER_MODELS } from './controllers/v0/model.index';
   const port = process.env.PORT || 8080;
 
   // Define allowed origins
-  const allowedOrigins = ['aff2e432476004eaaa05153ce7e87f92-2065947421.us-east-1.elb.amazonaws.com', 'a763c4ab5f15543c5a1a6ecc02a37b4a-1656197018.us-east-1.elb.amazonaws.com'];
+  const allowedOrigins = [
+    'http://aff2e432476004eaaa05153ce7e87f92-2065947421.us-east-1.elb.amazonaws.com/',
+    'http://another-allowed-origin.com'
+  ];
+
   const corsOptions = {
-    origin: (origin: any, callback: any) => {
+    origin: (origin:any, callback:any) => {
       if (allowedOrigins.includes(origin) || !origin) {
         callback(null, true);
       } else {
         callback(new Error('Not allowed by CORS'));
       }
-    }
+    },
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true,
   };
 
   app.use(bodyParser.json());
